73fe64769c2e7856205588e6d2e1dc8551b1a6e5
Jesper Jarlskov <jesper@jarlskov.dk>
2026-01-06 23:09:48 +0100

    Create Tag entity and link it to Recipe with ManyToMany relationship

--- src/Repository/RecipeRepository.php
+++ src/Repository/RecipeRepository.php
@@ -18,6 +18,9 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class RecipeRepository extends ServiceEntityRepository
 {
+    /**
+     * @param ManagerRegistry $registry
+     */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Recipe::class);
